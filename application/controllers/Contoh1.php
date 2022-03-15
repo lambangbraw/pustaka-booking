066254b96314365711d92d82f3152d713e44c9f8
lambangbraw <herlambang.braw@gmail.com>
2022-03-15 10:24:07 +0700

    panambahan controller contoh1.php

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,14 @@
+<?php
+
+class Contoh1 extends CI_Controller {
+
+	
+	public function index()
+	{
+		echo "<h1>Biodata</h1>";
+        echo"
+        Perkenalkan Nama Saya Herlambang
+        
+        ";
+	}
+}
